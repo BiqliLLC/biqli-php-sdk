168bdc804d5e89372bbf486ae3680c62f7a8c76c
BiqliLLC <contact@biq.li>
2026-09-15 17:06:32 +0000

    Release v1.0.2

--- src/Version.php
+++ src/Version.php
@@ -8,7 +8,7 @@ final class Version
 {
     public const SDK_NAME = 'biqli/sdk';
 
-    public const SDK_VERSION = '1.0.1';
+    public const SDK_VERSION = '1.0.2';
 
     private function __construct()
     {
